67bba60e44b2fda5df7b4051cdf0554827459efb
rahimuj570 <rahimujjaman2002@gmail.com>
2025-04-22 19:09:06 +0600

    server created and listining successfully 🌍

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
 	"github.com/rahimuj570/go_net-http_BLOG_REST_API/config"
+	"github.com/rahimuj570/go_net-http_BLOG_REST_API/routes"
 )
 
 func main() {
 	//Load Env
 	config.LoadEnv()
-	// mux := http.NewServeMux()
+	router := routes.SetupRouter()
 
-	//Configuring Server
-	// server := &http.Server{
-	// 	Addr: ":" + os.Getenv("PORT"),
-	// 	Handler: mux,
+	// Configuring Server
+	server := &http.Server{
+		Addr:         ":" + os.Getenv("PORT"),
+		Handler:      router,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+	}
 
-	// }
+	//Listining Server
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Server Error: ", err)
+	}
 }
--- routes/router.go
+++ routes/router.go
@@ -8,7 +8,7 @@ import (
 func SetupRouter() http.Handler {
 	mux := http.NewServeMux()
 
-	http.HandleFunc("GET /test", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Server is listining")
 	})
 
