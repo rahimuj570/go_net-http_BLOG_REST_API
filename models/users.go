67d1481277b7c360d772edc7883bdbf5c6888c97
rahimuj570 <rahimujjaman2002@gmail.com>
2025-04-24 19:15:40 +0600

    user model updated

--- models/users.go
+++ models/users.go
@@ -1,9 +1,11 @@
 package models
 
+import "github.com/rahimuj570/go_net-http_BLOG_REST_API/enums"
+
 type User struct {
 	ID       int
 	Name     string
 	Email    string
 	Password string
-	Role     string
+	Role     enums.Role
 }
