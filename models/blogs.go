84f01089b3a8b8d18a174373a177df4ad40caeac
rahimuj570 <rahimujjaman2002@gmail.com>
2025-04-24 19:17:29 +0600

    blog model updated

--- models/blogs.go
+++ models/blogs.go
@@ -1,9 +1,12 @@
 package models
 
+import "time"
+
 type Blog struct {
-	ID         int
-	Title      string
-	Content    string
-	AuthorID   int
-	CategoryID int
+	ID          int
+	Title       string
+	Content     string
+	AuthorID    int
+	CategoryID  int
+	CreatedDate time.Time
 }
